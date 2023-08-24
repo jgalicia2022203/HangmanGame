43da39bcc32201a1af795405186b9c9d97df6944
jgalicia2022203 <134737025+jgalicia2022203@users.noreply.github.com>
2023-08-24 14:58:08 -0600

    Adding of Random Words, generateLetterButtons and HandleLetterClick functions on JavaScript file

--- hangman.js
+++ hangman.js
@@ -38,3 +38,51 @@ const maxAttempts = 7;
 let attemptsRemaining = maxAttempts;
 let selectedWord = selectRandomWord(words);
 let gameInProgress = false;
+
+function selectRandomWord(words) {
+	const filteredWords = words.filter((word) => word.length <= 15);
+
+	if (filteredWords.length === 0) {
+		// No words match the filter criteria
+		return '';
+	}
+
+	const randomIndex = Math.floor(Math.random() * filteredWords.length);
+	return filteredWords[randomIndex];
+}
+
+function generateLetterButtons() {
+	for (let letter of alphabet) {
+		const button = document.createElement('button');
+		button.textContent = letter;
+		buttonsContainer.appendChild(button);
+
+		button.addEventListener('click', () => handleLetterClick(letter));
+	}
+}
+
+function handleLetterClick(letter) {
+	if (!gameInProgress) {
+		return;
+	}
+
+	if (usedLetters.includes(letter)) {
+		return;
+	}
+
+	usedLetters.push(letter);
+	updateUsedLetters();
+
+	if (selectedWord.includes(letter)) {
+		updateWordDisplay();
+	} else {
+		attemptsRemaining--;
+		updateAttemptsRemaining();
+	}
+
+	checkGameStatus();
+
+	if (!wordContainer.textContent.includes('_')) {
+		endGame(true);
+	}
+}
