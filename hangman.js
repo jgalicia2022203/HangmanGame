356d857263771dbfab8b7c0c680f2fd2173c3e39
jgalicia2022203 <134737025+jgalicia2022203@users.noreply.github.com>
2023-08-24 13:08:28 -0600

    Creation and connection of Files, and creation of JavaScript variables

---
+++ hangman.js
@@ -0,0 +1,29 @@
+const words = [
+	'programming',
+	'javascript',
+	'development',
+	'guatemala',
+	'computer',
+	'technology',
+	'application',
+	'learning',
+	'project',
+	'student',
+	'code',
+	'web',
+	'game',
+	'programmer',
+	'application',
+];
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+const buttonsContainer = document.getElementById('buttons-container');
+const usedLettersContainer = document.getElementById('used-letters');
+const attemptsRemainingContainer =
+	document.getElementById('attempts-remaining');
+const wordContainer = document.getElementById('word-container');
+const restartButton = document.getElementById('restart-button');
+const usedLetters = [];
+const maxAttempts = 7;
+let attemptsRemaining = maxAttempts;
+let selectedWord = selectRandomWord(words);
+let gameInProgress = false;
