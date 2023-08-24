4c5722157761df8fef74183f8bcd3b7a59b44f32
sramirez-2022506@kinal.edu.gt <sramirez-2022506@kinal.edu.gt>
2023-08-24 15:35:19 -0600

    Functions enableLetterButtons, endGame, disableLetterButtons, restartGame, initializeGame added on JavaScript Files

--- hangman.js
+++ hangman.js
@@ -38,3 +38,141 @@ const maxAttempts = 7;
 let attemptsRemaining = maxAttempts;
 let selectedWord = selectRandomWord(words);
 let gameInProgress = false;
+
+function selectRandomWord(words) {
+	const filteredWords = words.filter((word) => word.length <= 15);
+
+	if (filteredWords.length === 0) {
+		// No words match the filter criteria
+		return '';
+	}
+
+	const randomIndex = Math.floor(Math.random() * filteredWords.length);
+	return filteredWords[randomIndex];
+}
+
+function generateLetterButtons() {
+	for (let letter of alphabet) {
+		const button = document.createElement('button');
+		button.textContent = letter;
+		buttonsContainer.appendChild(button);
+
+		button.addEventListener('click', () => handleLetterClick(letter));
+	}
+}
+
+function handleLetterClick(letter) {
+	if (!gameInProgress) {
+		return;
+	}
+
+	if (usedLetters.includes(letter)) {
+		return;
+	}
+
+	usedLetters.push(letter);
+	updateUsedLetters();
+
+	if (selectedWord.includes(letter)) {
+		updateWordDisplay();
+	} else {
+		attemptsRemaining--;
+		updateAttemptsRemaining();
+	}
+
+	checkGameStatus();
+
+	if (!wordContainer.textContent.includes('_')) {
+		endGame(true);
+	}
+
+	
+}
+
+function enableLetterButtons() {
+
+    buttonsContainer.querySelectorAll('button').forEach((button) => {
+
+        button.disabled = false;
+
+    });
+
+}
+
+
+
+
+function endGame(playerWon) {
+
+    if (playerWon) {
+
+        wordContainer.textContent = 'Congratulations, you won!';
+
+    } else {
+
+        wordContainer.textContent = `Game over! The correct word was: ${selectedWord}`;
+
+    }
+
+
+
+
+    disableLetterButtons();
+
+    restartButton.disabled = true;
+
+
+
+
+    gameInProgress = false;
+
+}
+
+
+
+
+function restartGame() {
+
+    usedLetters.length = 0;
+
+    attemptsRemaining = maxAttempts;
+
+    selectedWord = selectRandomWord(words);
+
+    updateUsedLetters();
+
+    updateAttemptsRemaining();
+
+    updateWordDisplay();
+
+    wordContainer.textContent = '';
+
+    enableLetterButtons();
+
+
+
+
+    gameInProgress = true;
+
+}
+
+
+
+
+function initializeGame() {
+
+    generateLetterButtons();
+
+    restartGame();
+
+}
+
+
+
+
+restartButton.addEventListener('click', restartGame);
+
+
+
+
+initializeGame();
